c48e8dc05554e10f910aea1a2703ef6dd0ade70f
Túlio Estrela Marques <tulioestrela@hotmail.com>
2023-04-03 08:57:39 -0300

    Update ExtratorConteudoImdb.java

--- src/ExtratorConteudoImdb.java
+++ src/ExtratorConteudoImdb.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -6,8 +7,19 @@ public class ExtratorConteudoImdb implements ExtratorConteudo {
         // extrair só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeAtributos = parser.parse(json);
-        
-        return listaDeAtributos.stream()
-        .map(atributos -> new Conteudo(atributos.get("title"), atributos.get("image"))).toList();
+
+        List<Conteudo> conteudos = new ArrayList<>();
+
+        // popular a lista de conteudos
+        for (Map<String, String> atributos : listaDeAtributos) {
+            String titulo = atributos.get("title");
+            String urlImagem = atributos.get("image");
+
+            var conteudo = new Conteudo(titulo, urlImagem);
+
+            conteudos.add(conteudo);
+        }
+
+        return conteudos;
     }
 }
