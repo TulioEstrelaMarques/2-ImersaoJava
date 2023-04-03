e05a8a3e23a6696a03479217239576b6e091e922
Túlio Estrela Marques <tulioestrela@hotmail.com>
2023-04-03 10:07:56 -0300

    de volta pro futuro

--- src/App.java
+++ src/App.java
@@ -1,32 +1,41 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // fazer uma conexão HTTP e buscar os top 10 filmes ou séries;
         // System.getenv() para pegar variaveis de ambiente
 
-        String url =
-                // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // String url =
+        // ="https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 
-                // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        // String url =
+        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
 
-                 "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
+        // String url =
+        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
 
-                // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
 
-                //"https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD-JamesWebbSpaceTelescope.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        var http = new ClienteHttp();
-        String json = http.buscaDados(url);
-        
-        ExtratorConteudoNasa extrator = new ExtratorConteudoNasa();
-        List<Conteudo> conteudos = extrator.extraiConteudo(json);
-
-        // exibir e manipular os dados para imagem...
+        // exibir e manipular os dados para filmes...
         /*
          * for (Map<String, String> filme : listaDeFilmes) {
          * System.out.println(filme.get("title"));
@@ -36,13 +45,14 @@ public class App {
          */
         var diretorio = new File("figure/");
         diretorio.mkdir();
-        for (int i = 0; i < conteudos.size(); i++) {
-            Conteudo conteudo = conteudos.get(i);
+        for (int i = 0; i < listaDeFilmes.size(); i++) {
+            Map<String, String> filme = listaDeFilmes.get(i);
 
-            double classificacao = 8;// ouble.parseDouble(conteudo.get("imDbRating"));
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
             String textoFigure;
             InputStream avalia;
-
             if (classificacao >= 8.0) {
                 textoFigure = "TOPZERA!!!";
                 avalia = new FileInputStream(new File("sobrepor/like.png"));
@@ -50,8 +60,8 @@ public class App {
                 textoFigure = "HMMMMM...";
                 avalia = new FileInputStream(new File("sobrepor/hmmm.png"));
             }
-            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
-            String nameFile = "figure/" + conteudo.getTitulo() + ".png";
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nameFile = "figure/" + titulo + ".png";
 
             var figurinhas = new Figure();
             figurinhas.create(inputStream, nameFile, textoFigure, avalia);
